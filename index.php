9e38f45a739235a721d1a9ad183abcd99cea96ee
Angus <angus.goody@gmail.com>
2020-10-10 23:15:25 +0100

    Added length slider

--- index.php
+++ index.php
@@ -26,15 +26,20 @@ include_once include_local_file("/includes/a_config.php");
       </div>
       <div class="columns is-centered is-mobile mt-5">
         <!--Generated password label -->
-        <div class="column is-6-desktop is-7-tablet is-6-mobile is-centered has-background-light" style="border-radius: 4px">
-          <h4 class="is-size-4" style="font-family: 'Cousine', monospace;">aasjhd23413lsd</h4>
+        <div class="column is-6-desktop is-7-tablet is-6-mobile is-centered has-background-secure border3">
+          <h4 class="is-size-4 has-text-light" style="font-family: 'Cousine', monospace;">aasjhd23413lsd</h4>
         </div>
       </div>
 
-      <div class="columns is-centered">
+      <div class="columns is-centered mt-5">
         <!--Slider box -->
-        <div class="column is-6 is-centered">
-          <input type="range" min="1" max="100" value="50" class="slider">
+        <div class="column is-6-desktop is-7-tablet is-6-mobile is-centered has-background-light border3">
+          <h4 class="is-size-5 mb-1">Length: 25</h4>
+          <input type="range" min="1" max="100" value="50" class="slider" style="width: 100%">
+          <hr>
+          <div class="has-text-centered">
+            <button class="button">More</button>
+          </div>
         </div>
       </div>
     </div>
