6ed3364d645658f81983bc08789019506ff3ccf1
Angus <angus.goody@gmail.com>
2020-10-21 16:20:30 +0100

    Added overflow for password and copy and refresh buttons

--- index.php
+++ index.php
@@ -27,24 +27,84 @@ include_once include_local_file("/includes/a_config.php");
       <div class="columns is-centered is-mobile mt-5">
         <!--Generated password label -->
         <div class="column is-6-desktop is-7-tablet is-6-mobile is-centered has-background-secure border3">
-          <h4 class="is-size-4 has-text-light" style="font-family: 'Cousine', monospace;">aasjhd23413lsd</h4>
+          <div class="columns">
+            <div class="column is-9">
+              <h4 style="white-space: nowrap;overflow: hidden; overflow-x: scroll;
+              " id="passwordView" class="is-size-4 has-text-light" style="font-family: 'Cousine', monospace;">aasjhd23413lsd</h4>
+            </div>
+            <div class="column is-3">
+              <!--Level for buttons -->
+              <div class="level">
+                <div class="level-item">
+                  <button class="button"><span class="icon"><i class="far fa-copy"></i></span></button>
+                </div>
+                <div class="level-item">
+                  <button class="button"><span class="icon"><i class="fas fa-sync-alt"></i></span></button>
+                </div>
+              </div>
+            </div>
+          </div>
+          
         </div>
       </div>
 
       <div class="columns is-centered mt-5">
         <!--Slider box -->
         <div class="column is-6-desktop is-7-tablet is-6-mobile is-centered has-background-light border3">
-          <h4 class="is-size-5 mb-1">Length: 25</h4>
-          <input type="range" min="1" max="100" value="50" class="slider" style="width: 100%">
+          
+          <h4 id="lengthLabel" class="is-size-5 mb-1">Length: 25</h4>
+          <input id="lengthSlider" type="range" min="5" max="50" value="12" class="slider" style="width: 100%">
           <hr>
-          <div class="has-text-centered">
-            <button class="button">More</button>
-          </div>
         </div>
       </div>
     </div>
   </div>
   <!-- Footer -->
   <? include_once include_local_file("/includes/footer.php");?>
+  <!--Scripts-->
+  <script type="text/javascript">
+    
+    /*
+     * Will generate the complex password
+     */
+    function generate(length){
+      var password="";
+      //All numbers and letters
+      var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+      for ( var i = 0; i < length; i++ ) {
+        password += possible.charAt(Math.floor(Math.random() * possible.length));
+      }
+      return password;
+    }
+
+    /*
+     * Will generate the complex password
+     * as well as updating the correct labels
+     */
+    function update(length){
+      //Update the password label with a generated password
+      $("#passwordView").text(generate(length));
+      //Update the length label
+      let lengthContent="Length: "+length;
+      $("#lengthLabel").text(lengthContent);
+    }
+
+    //Whenever slider is moved
+    $("#lengthSlider").on("input change", function() {
+      //Get the slider value
+      var value = this.value;
+      //Update
+      update(value);
+    });
+
+    //First calls (when page loads)
+    $( document ).ready(function() {
+       //Update
+       var val = document.getElementById("lengthSlider").value
+       update(val); 
+    });
+
+
+  </script>
 </body>
 </html>
